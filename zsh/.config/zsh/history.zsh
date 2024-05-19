0c0ddfa436c2c153edd580dc82ead494d2c70bd0
Stavros <snikoletopoulos@hotmail.com>
2024-05-19 22:54:50 +0300

    Configure zsh history

---
+++ zsh/.config/zsh/history.zsh
@@ -0,0 +1,13 @@
+HISTSIZE=5000
+HISTFILE=~/.zsh_history
+SAVEHIST=$HISTSIZE
+HISTDUP=erase
+
+setopt appendhistory
+setopt sharehistory
+setopt hist_ignore_space
+setopt hist_ignore_all_dups
+setopt hist_save_no_dups
+
+bindkey '^p' history-search-backward
+bindkey '^n' history-search-forward
