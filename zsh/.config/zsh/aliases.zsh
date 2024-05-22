14f90127817731db93229900b42c1f652ec82c09
Stavros <snikoletopoulos@hotmail.com>
2024-05-23 01:22:23 +0300

    Add zsh aliases for exa and navigation

--- zsh/.config/zsh/aliases.zsh
+++ zsh/.config/zsh/aliases.zsh
@@ -1,5 +1,8 @@
-alias ls="lsd"
-
-alias dev="cd ~/Developer"
-
+alias ls="eza --group-directories-first --icons --oneline"
+alias lsl="eza --group-directories-first --icons --long --git"
+alias lst="eza --group-directories-first --icons --tree"
 alias nv="nvim"
+
+alias ..="cd .."
+alias ...="cd ../.."
+alias dev="cd ~/Developer"
