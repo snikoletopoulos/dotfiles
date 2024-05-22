f7444a8874330cf37e2676d6f0607549818dbd28
Stavros <snikoletopoulos@hotmail.com>
2024-05-22 20:19:43 +0300

    Move zsh aliases to functions and add navigation with ls command

--- zsh/.config/zsh/aliases.zsh
+++ zsh/.config/zsh/aliases.zsh
@@ -1,7 +1,5 @@
 alias ls="lsd"
 
-alias zsh_config="nvim ~/dotfiles/zsh/.zshrc"
-alias nvim_config="cd ~/.config/nvim/ && nvim"
 alias dev="cd ~/Developer"
 
 alias nv="nvim"
--- zsh/.config/zsh/functions.sh
+++ zsh/.config/zsh/functions.sh
@@ -1,3 +1,22 @@
 google() {
 	open "https://www.google.com/search?q=$*"
 }
+
+cx() { cd "$@" && ls; }
+zx() { z "$@" && ls; }
+
+dotconfig() {
+	cd ~/dotfiles || return
+	nv
+	cd - || return
+}
+
+nvimconfig() {
+	cd ~/dotfiles/nvim || return
+	nv
+	cd - || return
+}
+
+serveo() {
+	ssh -R stavros.serveo.net:80:localhost:${1:-3000} serveo.net
+}
