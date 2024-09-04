3cdec8ddcc660a88c9b66517674cea81396cc659
Stavros <snikoletopoulos@hotmail.com>
2024-09-04 22:19:29 +0300

    Move yazi config to separate file

---
+++ zsh/.config/zsh/yazi.zsh
@@ -0,0 +1,12 @@
+unalias ya
+unalias yy
+
+yy() {
+	local tmp
+	tmp="$(mktemp -t "yazi-cwd.XXXXXX")"
+	yazi "$@" --cwd-file="$tmp"
+	if cwd="$(cat -- "$tmp")" && [ -n "$cwd" ] && [ "$cwd" != "$PWD" ]; then
+		builtin cd -- "$cwd" || return
+	fi
+	rm -f -- "$tmp"
+}
