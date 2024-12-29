c0c51cc49a58dafc94510175558ad938efeff80d
Stavros Nikoletopoulos <snikoletopoulos@hotmail.com>
2024-12-29 22:50:13 +0100

    Enable transparent background in wezterm

--- wezterm/.wezterm.lua
+++ wezterm/.wezterm.lua
@@ -11,6 +11,8 @@ config.term = "wezterm"
 
 config.hide_tab_bar_if_only_one_tab = true
 config.window_decorations = "RESIZE"
+config.window_background_opacity = 0.87
+config.macos_window_background_blur = 20
 config.native_macos_fullscreen_mode = true
 
 config.window_padding = {
