3567a6e3215eec30f31c923d6409617ee56aee4b
Stavros Nikoletopoulos <snikoletopoulos@hotmail.com>
2025-01-03 01:39:41 +0100

    Create install script

---
+++ install.sh
@@ -0,0 +1,29 @@
+#! /bin/bash
+
+log() {
+	printf "\n\e[1;34m====> %s <====\e[0m\n\n" "$1"
+}
+
+# Install Homebrew
+if ! [ -x "$(command -v brew)" ]; then
+	log "Installing Homebrew"
+	/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+else
+	log "Homebrew is already installed"
+fi
+brew update
+
+# Install Homebrew packages
+log "Installing Homebrew packages"
+brew bundle
+
+# Setup dotfiles
+log "Setting up dotfiles"
+stow */
+
+# Setup yazi
+log "Setting up yazi"
+ya pack -i
+if [ ! -d "$HOME/.config/yazi/plugins/yatline-catppuccin.yazi" ]; then
+	git clone https://github.com/imsi32/yatline-catppuccin.yazi.git "$HOME/.config/yazi/plugins/yatline-catppuccin.yazi"
+fi
