ae15eea494d7e7536c42c20c3a774cccc369872d
Stavros Nikoletopoulos <snikoletopoulos@hotmail.com>
2025-04-10 00:50:19 +0200

    Add minimum tmux popup for fzf-tab

--- zsh/dot-config/zsh/completions.zsh
+++ zsh/dot-config/zsh/completions.zsh
@@ -5,6 +5,7 @@ zstyle ':completion:*:descriptions' format '[%d]'
 zstyle ":fzf-tab:complete:cd:*" fzf-preview 'eza --group-directories-first --color=always --long --no-user --no-time --icons --git $realpath'
 zstyle ":fzf-tab:complete:nvim:*" fzf-preview '[ -f $realpath ] && bat --color=always --style=numbers $realpath || eza --group-directories-first --color=always --long --no-user --no-time --icons --git $realpath'
 zstyle ':fzf-tab:complete:z:*' fzf-preview 'eza --group-directories-first --color=always --long --no-user --no-time --icons --git $realpath'
+zstyle ':fzf-tab:complete:(cd|nvim|z|kill|ps|systemctl|brew-(install|uninstall|search|info)):*' popup-min-size 100 8
 
 # fzf-tab: kill
 zstyle ':completion:*:*:*:*:processes' command "ps -u $USER -o pid,user,comm -w -w"
