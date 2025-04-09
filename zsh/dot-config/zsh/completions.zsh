80ffcfee58756343578474b92facb714ae5bbfe3
Stavros Nikoletopoulos <snikoletopoulos@hotmail.com>
2025-04-10 00:38:25 +0200

    Configure fzf-tab completions

--- zsh/dot-config/zsh/completions.zsh
+++ zsh/dot-config/zsh/completions.zsh
@@ -1,12 +1,21 @@
-zstyle ":completion:*" matcher-list "m:{a-z}={A-Za-z}"
-zstyle ":completion:*" list-colors "${(s.:.)LS_COLORS}"
-zstyle ":completion:*" menu no
-
 # fzf-tab
-zstyle ":fzf-tab:complete:cd:*" fzf-preview 'ls --color $realpath'
-zstyle ":fzf-tab:complete:lsd:*" fzf-preview 'ls --color $realpath'
-zstyle ":fzf-tab:complete:nvim:*" fzf-preview '[ -f $realpath ] && bat  --color=always --style=numbers $realpath || ls --color $realpath'
-zstyle ':fzf-tab:complete:z:*' fzf-preview 'ls --color $realpath'
+zstyle ":completion:*" list-colors '${(s.:.)LS_COLORS}'
+zstyle ":completion:*" menu no
+zstyle ':completion:*:descriptions' format '[%d]'
+zstyle ":fzf-tab:complete:cd:*" fzf-preview 'eza --group-directories-first --color=always --long --no-user --no-time --icons --git $realpath'
+zstyle ":fzf-tab:complete:nvim:*" fzf-preview '[ -f $realpath ] && bat --color=always --style=numbers $realpath || eza --group-directories-first --color=always --long --no-user --no-time --icons --git $realpath'
+zstyle ':fzf-tab:complete:z:*' fzf-preview 'eza --group-directories-first --color=always --long --no-user --no-time --icons --git $realpath'
+
+# fzf-tab: kill
+zstyle ':completion:*:*:*:*:processes' command "ps -u $USER -o pid,user,comm -w -w"
+zstyle ':fzf-tab:complete:(kill|ps):argument-rest' fzf-preview '[[ $group == "[process ID]" ]] && ps --pid=$word -o cmd --no-headers -w -w'
+zstyle ':fzf-tab:complete:(kill|ps):argument-rest' fzf-flags --preview-window=down:3:wrap
+
+# fzr-tab: systemd
+zstyle ':fzf-tab:complete:systemctl-*:*' fzf-preview 'SYSTEMD_COLORS=1 systemctl status $word'
+
+# fzf-tab: homebrew
+zstyle ':fzf-tab:complete:brew-(install|uninstall|search|info):*-argument-rest' fzf-preview 'brew info $word'
 
 # Docker
 zstyle ':completion:*:*:docker:*' option-stacking yes
