a94a65b21dd5e01fef1623f8b6e8675e127d5392
Stavros Nikoletopoulos <snikoletopoulos@hotmail.com>
2025-06-29 23:56:59 +0200

    Move history setting to init

--- zsh/dot-config/zsh/init.zsh
+++ zsh/dot-config/zsh/init.zsh
@@ -6,3 +6,14 @@ setopt nomatch
 zle_highlight=('paste:none')
 
 bindkey -v
+# History
+HISTSIZE=5000
+HISTFILE=~/.zsh_history
+SAVEHIST=$HISTSIZE
+HISTDUP=erase
+
+setopt appendhistory
+setopt sharehistory
+setopt hist_ignore_space
+setopt hist_ignore_all_dups
+setopt hist_save_no_dups
