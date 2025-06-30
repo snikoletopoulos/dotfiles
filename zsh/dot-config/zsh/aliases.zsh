0ee01846a8647248081b52055bab459bf9a35527
Stavros Nikoletopoulos <snikoletopoulos@hotmail.com>
2025-06-30 20:35:42 +0200

    Fix rebase error

--- zsh/dot-config/zsh/aliases.zsh
+++ zsh/dot-config/zsh/aliases.zsh
@@ -2,5 +2,17 @@ alias ..="cd .."
 alias ...="cd ../.."
 alias dev="cd ~/Developer"
 
+alias nv="nvim"
+alias lg="lazygit"
+
+# eza
+alias ls="eza --group-directories-first --icons --oneline"
+alias lsl="eza --group-directories-first --icons --long --no-user --git"
+alias lst="eza --group-directories-first --icons --tree"
+
+# pnpm
+alias pn="pnpm"
+
+# tmuxinator
 alias mux="tmuxinator"
 alias muxstart="mux list -n | tail -n +2 | fzf | xargs tmuxinator start"
