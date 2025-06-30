8fb7d7ba3387b329de0f37ba90bd8a84209f184a
Stavros Nikoletopoulos <snikoletopoulos@hotmail.com>
2025-06-30 20:25:05 +0200

    Move variables to one file

--- zsh/dot-config/zsh/ccache.zsh
+++
@@ -1,4 +0,0 @@
-export CCACHE_SLOPPINESS=clang_index_store,file_stat_matches,include_file_ctime,include_file_mtime,ivfsoverlay,pch_defines,modules,system_headers,time_macros
-export CCACHE_FILECLONE=true
-export CCACHE_DEPEND=true
-export CCACHE_INODECACHE=true
--- zsh/dot-config/zsh/go.zsh
+++
@@ -1,2 +0,0 @@
-GOPATH=$HOME/go
-PATH=$PATH:/usr/local/go/bin:$GOPATH/bin
