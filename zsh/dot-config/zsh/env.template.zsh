3f5fa2499d18623a21956028ebba7b1787f3df97
Stavros Nikoletopoulos <snikoletopoulos@hotmail.com>
2026-01-04 13:50:10 +0100

    Add ngrok env variable and completions

--- zsh/dot-config/zsh/env.template.zsh
+++ zsh/dot-config/zsh/env.template.zsh
@@ -1,3 +1,4 @@
 export CHAT_GPT_API_KEY="op://Developer/ChatGPT API Key/credential"
 export ANTHROPIC_API_KEY="op://Developer/Anthropic API Key/credential"
 export SPOTIFY_CLIENT_ID="op://Developer/Spotify Client ID/credential"
+export NGROK_AUTHTOKEN="op://Developer/ngrok Auth token/credential"
